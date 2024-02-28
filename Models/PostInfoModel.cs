1bddc5760b7cb820d066a6f70cf5c5cefd794c7f
kumar-Akshaya <akshayidli1430@gmail.com>
2024-02-28 19:12:46 +0530

    Create and delete page view added.

---
+++ Models/PostInfoModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Twiblog.Models
+{
+    public class PostInfoModel
+    {
+        [Key]
+        public int? PostId { get; set; }
+
+        [Required]
+        public string PostedDate { get; set; }
+
+        [Required]
+        public string? PostEditedDate { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "You must enter at least 2 characters to be able to post this message")]
+        public string UserName { get; set; }
+
+        public string PostContent { get; set; }
+
+        public List<string>? Likes { get; set; }
+
+        public List<ReplyInfoModel> Replies { get; set; }
+    }
+}
